e41b083b2ec3d18f82a7d558c15b0e1c4ed89c75
Jean-Michel <jeanmichel.didon@gmail.com>
2013-06-04 10:58:22 +0200

    timeline.js : Gestion des 2 curseurs
    Ajout du 2ème curseur, gestion de la collision, récupération de lépisode

--- public/js/timeline.js
+++ public/js/timeline.js
@@ -1,86 +1,118 @@
 window.addEventListener('DOMContentLoaded', initialize, false);
 
 
-var retrieved_data 			= {};
-retrieved_data.moves 		= {};
-retrieved_data.houses 		= {};
-retrieved_data.places 		= {};
-retrieved_data.episodes 	= {};
-retrieved_data.characters 	= {};
+var retrieved_data 				= {};
+	retrieved_data.moves 		= {};
+	retrieved_data.houses 		= {};
+	retrieved_data.places 		= {};
+	retrieved_data.episodes 	= {};
+	retrieved_data.characters 	= {};
 
 
 /* Chargement des infos des épisodes */
 	$.get('api/get/moves', function(data)
 	{
 		retrieved_data.moves = JSON.parse(data);
-		console.log(retrieved_data.moves);
+		//console.log(retrieved_data.moves);
 	});
 
 	$.get('api/get/houses', function(data)
 	{
 		retrieved_data.houses = JSON.parse(data);
-		console.log(retrieved_data.houses);
+		//console.log(retrieved_data.houses);
 	});
 
 	$.get('api/get/places', function(data)
 	{
 		retrieved_data.places = JSON.parse(data);
-		console.log(retrieved_data.places);
+		//console.log(retrieved_data.places);
 	});
 
 	$.get('api/get/episodes', function(data)
 	{
 		retrieved_data.episodes = JSON.parse(data);
-		console.log(retrieved_data.episodes);
+		//console.log(retrieved_data.episodes);
 	});
 
 	$.get('api/get/characters', function(data)
 	{
 		retrieved_data.characters = JSON.parse(data);
-		console.log(retrieved_data.characters);
+		//console.log(retrieved_data.characters);
 	});
-
-
 
 
 /* Chargement de la timeline */
 	function initialize()
 	{
-		var screenWidth = screen.width;
-		console.log(screenWidth);
-		var graduations = Math.floor(screenWidth/30);
-		var initial_position = $('#cursor_begin').offset().left;
-		var episodeFound;
-		initial_position = Math.round(initial_position);
-		console.log(graduations);
+		var screenWidth 			= screen.width,
+			graduations 			= Math.floor(screenWidth/30),
+			initial_position_begin  = $('#cursor_begin').offset().left,
+			initial_position_end 	= $('#cursor_end').offset().left,
+			cursor_begin 			= document.getElementById('cursor_begin'),
+			cursor_end				= document.getElementById('cursor_end'),
+			episodeFound,
+			posX,
+			offset;
+	
+		// On prend la position de chaque élément et on l'enregistre dans l'attribut data-posX de chaque curseur
+			initial_position_begin  = Math.round(initial_position_begin);
+			initial_position_end	= Math.round(initial_position_end);
+			cursor_begin.setAttribute('data-posX', initial_position_begin);
+			cursor_end.setAttribute('data-posX', initial_position_end);
+		
+			$('#posBegin').text('Begin: ' + initial_position_begin);
+			$('#posEnd').text('End: ' + initial_position_end);
 
-		var posX, posY, offset;
-		$('#cursor_begin').draggable(
-	    {
-	    	grid: [graduations, 0],
-	    	snap: true,
-	    	snapMode: 'both',
-	    	containment: 'html',
-	        drag: function(){
-	            offset = $(this).offset();
+		
+		// Fonction pour rendre draggable les curseurs
+			$('.draggable').draggable(
+		    {
+		    	grid: [graduations, 0],
+		    	snap: true,
+		    	snapMode: 'both',
+		    	containment: 'html',
+		    	cursor: 'pointer',
+		        drag: function(){
+		            offset = $(this).offset();
+		            posX = offset.left;
+		            posX = Math.round(posX);
 
-	            posX = offset.left;
-	            posY = offset.top;
-	            posX = Math.round(posX);
-	            posY = Math.round(posY);
-
-	            $('#posX').text('X: ' + posX);
-	            $('#posY').text('Y: ' + posY);
-
-	            findEpisode(initial_position, posX, graduations);          
-	        }
-	    });
-
-	    //getInfosEpisode(episodeFound);    
+		        // On détecte quel est le curseur que l'on drag
+		            if(this.getAttribute('id') == 'cursor_begin')
+		            {
+		            	// On teste la valeur de posX du début par rapport à la fin
+		            	$('#posBegin').text('Begin: '+posX);
+		            	cursor_begin.setAttribute('data-posX', posX);
+		            	if(parseInt(cursor_begin.getAttribute('data-posX')) >= parseInt(cursor_end.getAttribute('data-posX')))
+		            	{
+		            		return false;
+		            	}
+		            	else
+		            	{
+		            		// Si c'est bon, on va chercher quel est l'épisode grâce aux données
+		            		findFirstEpisode(initial_position_begin, posX, graduations);
+		            	}
+		            	// End if
+		            }
+		            else
+		            {
+		        		// Idem pour le curseur de fin
+		            	$('#posEnd').text('End: '+posX);
+		            	cursor_end.setAttribute('data-posX', posX);
+		            	if(parseInt(cursor_begin.getAttribute('data-posX')) <= parseInt(cursor_end.getAttribute('data-posX')))
+		            	{
+		            		return false;
+		            	}
+		            	else
+		            	{
+		            		findLastEpisode(initial_position_end, posX, graduations);
+		            	}
+		            	// End if
+		            }
+		            // End if
+		        } // End drag
+		    }); // End draggable
 	}
-
-
-
 
 /* Canvas */
 	function changePersonagePosition()
@@ -109,24 +141,44 @@ retrieved_data.characters 	= {};
 	}
 
 
-
-
-/* Find episode */
-	function findEpisode(initial_position, posX, graduations)
+/* Find episodes */
+	function findFirstEpisode(initial_position, posX, graduations)
 	{
-		var episode = compteur = ((posX - initial_position)/graduations)+1;
+		var episode = Math.floor(((posX - initial_position)/graduations)+1); 
 		var season  = 1;
-		if (episode >= 10)
+		if (episode >= 11)
 		{
 			season++;
-			episode = episode - 9;
-			if (episode >= 10)
+			episode = episode - 10;
+			if (episode >= 11)
 			{
 				season++;
-				episode = episode - 9;
+				episode = episode - 10;
 			}
 		}
-		$('#identifier').text('S0'+season+'E'+episode);
+		$('#identifierBegin').text('Épisode de début : S0'+season+'E'+episode);
+		var episodeFound = 'S0'+season+'E'+episode;
+		return episodeFound;
+	}
+
+	function findLastEpisode(initial_position, posX, graduations)
+	{
+		var episode = Math.floor(((posX - initial_position)/graduations)+10);
+		/*console.log('initial_position : '+initial_position);
+		console.log('posX : '+posX);
+		console.log('episode : '+episode);*/
+		var season  = 3;
+		if (episode < 1)
+		{
+			season--;
+			episode = episode + 10;
+			if (episode < 1)
+			{
+				season--;
+				episode = episode + 10;
+			}
+		}
+		$('#identifierEnd').text('Épisode de fin : S0'+season+'E'+episode);
 		var episodeFound = 'S0'+season+'E'+episode;
 		return episodeFound;
 	}
